3274b1e6284fea3be36014b6acaa8c0e0b8a2f09
Sinoon <sinoon1218@gmail.com>
2014-09-24 18:50:35 +0800

    增加对于新赔的判断支持

--- a.js
+++ a.js
@@ -4,7 +4,7 @@ var fs = require("fs")
  * [a code源码]
  * @type {String}
  */
-var a = '[{"lotterytype":"SportteryNWDL","match":"105#4.04-4.04,3.0-3.2,4.02-4.02,4.57-4.57,3.71-3.71,4.6-4.6,3.91-3.91,4.68-4.68,4.01-4.01,3.22-3.22,3.23-3.23,4.76-4.76","option":"","filter":"555#12,22,23,33|888#1","sort":"428#2014-09-23_2_1_1","pass":"111#100_2","update_time":"2014-09-23 23:43:09","ver":1}]'
+var a = '[{"lotterytype":"SportteryNWDL","match":"100#1001,1008,1011|1001#8,17,23,34,35|1006#238,20,36,37,40,136,155,325|1007#24,44,131|1008#1,11,13,14,16,27,28,436,308,295,270,246,140,133,460|1009#357,384,463,465,480,498,679,1124,1132|1011#851,853|1010#19,21,29,80,101,213,217,323,327,328,329,330,333,334,335,336,346,373,1024|2029#3-4|1109#3-4","option":"","filter":"555#12,13,21,22,31|888#3","sort":"4259#2014-09-08_2_0_1","pass":"111#100_2","update_time":"2014-09-08 21:21:42","ver":1}]'
 
 /**
  * [json 解析源码]
@@ -32,11 +32,19 @@ var update_time = json[0]["update_time"]
 fs.readFile("code.json","utf-8",function (err,list){
 	var code = eval(list)
 	var _match = match.split("|")
+	// var _match = match.split("|")
+
 	// console.log(_match)
-	var s_match = _match[0].split("#")
+	for (var i = 0; i < _match.length; i++) {
+		var s_match = _match[i].split("#")
+		console.log(s_match)
+		// if( isNum(code , ) )
+	};
+	// var s_match = _match[0].split("#")
 	//分割完毕-105,3.0-3.2
-	// console.log(s_match)
-	console.log(isNum(code,"100"))
+	// console.log(_match)
+
+	// isNum(code,"100")
 	// console.log(code[0]["match"])
 })
 
@@ -48,10 +56,15 @@ fs.readFile("code.json","utf-8",function (err,list){
  */
 function isNum(code,num)
 {
-	var temp = code[0]["match"]["H985"]["child"];
-	for (var i = 0; i < temp.length; i++) {
-		if( temp[i] == "H" + num )
+	var temp_1 = code[0]["match"]["H985"]["child"];
+	for (var i = 0; i < temp_1.length; i++) {
+		if( temp_1[i] == "H" + num )
 			return false;
+	};
+	var temp_2 = code[0]["match"]["H9859"]["child"]
+	for (var i = 0; i < temp_2.length; i++) {
+		if( temp_2[i] == "H" + num )
+			return false
 	};
 	return true
 }
