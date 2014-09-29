cc17b83a2e20dd7f29f55ef98d2a261cca15ed6b
Sinoon <sinoon1218@gmail.com>
2014-09-29 21:23:20 +0800

    增加getModelName 获取模型作者与名字函数

--- base.js
+++ base.js
@@ -106,3 +106,24 @@ function getModel(mId,callback)
 }
 
 exports.getModel = getModel;
+
+function getModelName(mId,callback)
+{
+	request({url:'http://winner.okooo.com/model/info?mid=' + mId,headers:{'User-Agent':'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11',
+	  'Accept':'text/html;q=0.9,*/*;q=0.8',
+	  'Accept-Charset':'ISO-8859-1,utf-8;q=0.7,*;q=0.3',
+	  'Connection':'close',
+	  'Referer':'None'}}, function (err, res,body) {
+	  if (err) return err;
+
+	  var t = body.match("<h2 class=\"model_name\">(.*)<a href")
+	  // var a = "[" + t[1] + "]";
+	  var t = t[1].split("-");
+
+	  callback(t[0],t[1]);
+	  // 输出结果
+	});
+	
+}
+
+exports.getModelName = getModelName;
