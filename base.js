add2f5d292d7f56437778d630b00d5b66ad071c5
Sinoon <sinoon1218@gmail.com>
2014-09-29 21:04:45 +0800

    增加获取模型页面并提取出模型代码函数

--- base.js
+++ base.js
@@ -1,3 +1,5 @@
+var request = require('request');
+
 /**
  * [isNum 判断是否为自定义区间]
  * @param  {[json]}  code
@@ -85,3 +87,22 @@ function getSortName(code,point)
 	return code[0]["sort"]["S001"]["default"][point]
 }
 exports.getSortName = getSortName
+
+function getModel(mId,callback)
+{
+	request({url:'http://winner.okooo.com/model/info?mid=' + mId,headers:{'User-Agent':'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11',
+	  'Accept':'text/html;q=0.9,*/*;q=0.8',
+	  'Accept-Charset':'ISO-8859-1,utf-8;q=0.7,*;q=0.3',
+	  'Connection':'close',
+	  'Referer':'None'}}, function (err, res,body) {
+	  if (err) return err;
+
+	  var t = body.match("var methodlist = '(.*)'")
+	  var a = "[" + t[1] + "]";
+
+	  callback(a);
+	  // 输出结果
+	});
+}
+
+exports.getModel = getModel;
