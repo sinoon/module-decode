023b741db2d877038ea4ec349e490c696dece9e6
Sinoon <sinoon1218@gmail.com>
2014-09-30 18:56:27 +0800

    完成基本功能的编写

--- start.js
+++ start.js
@@ -2,12 +2,16 @@ var decode = require("./decode").decode;
 var base = require("./base")
 
 
-base.getModel("1411986036837",function (a){
+base.getModel("1412010961144",function (model,atthorName,modelName){
 
-decode(a,function (s1,filter,pass,length){
-	console.log(length)
-	var s1 = s1[1][0];
-
+decode(model,function (s1,filter,pass,length){
+	// console.log(length)
+	console.log("作者：",atthorName);
+	console.log("模型名字：",modelName);
+	// console.log("s1:",s1.length)
+	// console.log("s1",s1.length)
+	var s1 = s1[s1.length-1][0];
+	// console.log(s1)
 	for (var i = 1; i < s1.length; i++) {
 		
 		console.log(i,s1[i].length)
@@ -15,10 +19,10 @@ decode(a,function (s1,filter,pass,length){
 		console.log(s1[i])
 		console.log("==========down=========")
 	};
-	
-	console.log(filter[0][1])
 
-	console.log(pass)
+	// console.log(filter[0][1])
+
+	// console.log(pass)
 })
 
 })
