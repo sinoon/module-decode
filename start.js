0f03a44d834793b1dc5b175445a608b415e72851
Sinoon <sinoon1218@gmail.com>
2014-10-04 19:41:32 +0800

    完成显示测试

--- start.js
+++ start.js
@@ -1,28 +1,28 @@
 var decode = require("./decode").decode;
 var base = require("./base")
 
+var model;
 
-base.getModel("1412010961144",function (model,atthorName,modelName){
+base.getModel("1410426920114",function (model,atthorName,modelName){
 
-decode(model,function (s1,filter,pass,length){
-	// console.log(length)
-	console.log("作者：",atthorName);
-	console.log("模型名字：",modelName);
-	// console.log("s1:",s1.length)
-	// console.log("s1",s1.length)
-	var s1 = s1[s1.length-1][0];
-	// console.log(s1)
-	for (var i = 1; i < s1.length; i++) {
-		
-		console.log(i,s1[i].length)
-		console.log("=========top===========")
-		console.log(s1[i])
-		console.log("==========down=========")
-	};
+	decode(model,function (s1,filter,pass,length){
+		console.log("作者：",atthorName);
+		console.log("模型名字：",modelName);
+		for (var i = 0; i < s1.length; i++) {
+			// console.log(i)
+			var temp = s1.length;
+			// console.log(temp)
+			for (var i_1 = 0; i_1 < 3; i_1++) {
+				console.log(i_1)
+				console.log(s1[i][i_1])
+				console.log("==========")
+			};
+			console.log("-------------------")
+		};
 
-	// console.log(filter[0][1])
+		// console.log(filter[0][1])
 
-	// console.log(pass)
-})
+		// console.log(pass)
+	})
 
 })
